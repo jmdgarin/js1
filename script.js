fb0134aea0074cb85b7137e95724ae5e09155722
John Michael D. Garin <126480023+jmdgarin@users.noreply.github.com>
2023-04-01 23:37:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// 1
+
+
+// Create a function that will accept a number, if the number given by the user is an odd number, display "odd number". If even number, display "even number".
+
+// let number = prompt ("Enter your number");
+
+// function numberCheck (number){
+//     if (number) diko alam operator maam
+//     {
+//         console.log("odd number");
+//     }
+//     else{
+//         console.log("even number");
+//     }
+// }
+
+
+// Create a Switch Statement that accepts the colors: Black, Violet, and Pink.
+
+
+
+
+// Put the conditional statement inside the function.
+// Ensure that the proper NAMING STANDARD is followed.
+// Create a Switch Statement that accepts the colors: Black, Violet, and Pink.
+// Lowercase and Uppercase letters must be accepted
+
+
+// let color = prompt ("Enter your color");
+
+// switch (color) 
+// {
+//     case (color = "BLACK", "black"):
+//         console.log("Black");
+//         break;
+
+//     case (color = "Violet", "VIOLET"):
+//         console.log("Violet");
+//         break;
+
+//     case (color = "Pink", "PINK"):
+//         console.log("Pink");
+//         break;
+
+//     default:
+//     console.log("Invalid color");
+
+// }
+
+
+// Create a function that will accept a number, if the number given by the user is an odd number, display "odd number". If even number, display "even number".
+// Before logging an odd or even number, check the input of the user. If the user input is a string such as "one", "two", "or three", display unexpected input.
+// Try these arguments
+// 5
+// 10
+// "8"
+// "20"
+// "seven"
+// "Fifteen"
+// true
+// null
+// undefined
+// NaN
+
+let userInput = prompt("Please enter your email or username:");
